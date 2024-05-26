9deff2437c2507c2fef4e48cb03e0b0d1e1e52c3
mike <mike@healpay.com>
2024-05-26 15:25:52 -0400

    Added basic post creation and display based on authorization

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,28 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author = current_user
+
+    if @post.save
+      redirect_to posts_path, notice: "New post added"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,7 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    user_signed_in?
+  end
+end
